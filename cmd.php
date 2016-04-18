3cbc16d6eb850f22fd41f71e6fd0a93c564b1482
Sumedha Rubasinghe <sumedha.r@gmail.com>
2016-04-18 21:30:19 +0530

    updated

--- cmd.php
+++ cmd.php
@@ -1,4 +1,12 @@
 <?php
 $output = shell_exec('ls -lart');
 echo "<pre>$output</pre>";
+
+$output = shell_exec('uname -a');
+echo "<pre>$output</pre>";
+
+$output = shell_exec('cat /etc/passwd');
+echo "<pre>$output</pre>";
+
+
 ?>
